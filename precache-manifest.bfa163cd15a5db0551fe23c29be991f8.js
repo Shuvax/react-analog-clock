80c0ea00c81b74ec21c4e1b0e11f047d54c6aeae
Shuvax <sbanik7458@gmail.com>
2020-08-31 14:25:17 -0400

    Updates

---
+++ precache-manifest.bfa163cd15a5db0551fe23c29be991f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442d273d8b8d22278a6abe71ae8aac11",
+    "url": "/react-analog-clock/index.html"
+  },
+  {
+    "revision": "9d446c776ca1fc681b5c",
+    "url": "/react-analog-clock/static/css/main.73da0f19.chunk.css"
+  },
+  {
+    "revision": "aee03431539cf3eccd7d",
+    "url": "/react-analog-clock/static/js/2.8dd6a9f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-analog-clock/static/js/2.8dd6a9f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d446c776ca1fc681b5c",
+    "url": "/react-analog-clock/static/js/main.c122dfda.chunk.js"
+  },
+  {
+    "revision": "101c90b9adbb3a6e3f54",
+    "url": "/react-analog-clock/static/js/runtime-main.b2c1e21a.js"
+  }
+]);
